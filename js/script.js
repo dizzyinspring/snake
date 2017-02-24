beadc88309d1b84b6f78a6a3b9ec676a2687a24f
dizzyinspring <happegg@163.com>
2017-02-25 00:06:03 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
  * Created by Administrator on 2017/2/23.
  */
 //if(navigator.userAgent.match(/(iPhone|iPod|Android|ios)/i)){
-    alert('131')
+    alert('141')
     for(var i = 0;i<5;i++){
        console.log(document.getElementsByTagName('button')[i]);
-        document.getElementsByTagName('button')[i].setAttribute('-webkit-appearence','none');
+        document.getElementsByTagName('button')[i].setAttribute('-webkit-appearance','none');
         
     };for(i = 0;i<4;i++)
         document.getElementsByClassName('directbut')[i].removeAttribute('onclick');alert('3');
